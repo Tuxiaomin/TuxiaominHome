4339ab8608f3049014acd3055042b23749a17ed8
Tuxiaomin <1135084184@qq.com>
2019-06-11 14:52:24 +0800

    userActionControlle

---
+++ src/main/java/com/min/blog/modules/app/controller/UserActionController.java
@@ -0,0 +1,20 @@
+package com.min.blog.modules.app.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author min
+ * @since 2019-05-07
+ */
+@RestController
+@RequestMapping("/user-action")
+public class UserActionController {
+
+}
